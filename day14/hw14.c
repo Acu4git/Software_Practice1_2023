8d95ef19af79e06e6e54251b388c284181a1fdcc
Eishu Kawaguchi <sikewa-e.k@outlook.jp>
2024-01-16 22:43:58 +0900

    aaa

--- day14/hw14.c
+++ day14/hw14.c
@@ -3,6 +3,8 @@
 
 int main(int argc, char *argv[]) {
   FILE *fpi, *fpo;
+  int meta = 0;
+  int ch;
 
   if(argc != 3) {
     fprintf(stderr, "Illegal number of argument.\n");
@@ -18,4 +20,55 @@ int main(int argc, char *argv[]) {
     fprintf(stderr, "Can't open output file <%s>.\n", argv[2]);
     return -1;
   }
+
+  while((ch = fgetc(fpi)) != EOF) {
+    if(meta == 1) {
+      switch(ch) {
+        case '\\':
+        case '#':
+        case '$':
+        case '%':
+        case '&':
+        case '_':
+        case '{':
+        case '}':
+          fputc(ch, fpo);
+          break;
+      }
+      meta = 0;
+    } else if(meta == 2) {
+      switch(ch) {
+        case '\'':
+          fputc('"', fpo);
+          break;
+        default:
+          fputc('\'', fpo);
+          fputc(ch, fpo);
+          break;
+      }
+      meta = 0;
+    } else if(ch == '\\') {
+      meta = 1;
+    } else if(ch == '\'') {
+      meta = 2;
+    } else {
+      switch(ch) {
+        case '\\':
+        case '#':
+        case '$':
+        case '%':
+        case '&':
+        case '_':
+        case '{':
+        case '}':
+        case '"':
+          break;
+        default:
+          fputc(ch, fpo);
+          break;
+      }
+    }
+  }
+
+  return 0;
 }
